2c8aba74a0df7b33f3de5dc4499b7b47ed16b706
Kelly Salamack <kellysalamackdesign@gmail.com>
2019-08-03 11:41:10 -0400

    Updates

---
+++ precache-manifest.993568108d14a798c5a64e7bffdf58c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521b0b31b7b056f6ed440a8c54880cd3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "114430c69af093fd4b8f",
+    "url": "/clicky-game/static/css/main.6cffb5bc.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "114430c69af093fd4b8f",
+    "url": "/clicky-game/static/js/main.c479176b.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
